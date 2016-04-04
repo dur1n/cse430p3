80deed3c84eabe4e5d75d0e23cc85474a645f44a
dur1n <tqtuan1982@gmail.com>
2016-04-04 13:13:16 -0700

    Added files via upload

---
+++ memCall_test.c
@@ -0,0 +1,22 @@
+// mem_user.c test program
+
+#include <linux/unistd.h>
+#include <linux/stdio.h>
+
+#define __NR_my_syscall 359
+
+main(int argc, char *argv[]) {
+{
+    int addr = atoi(argv[1]);  // Converting a string argument to an integer 
+    int arg2 = atoi(argv[2]);  
+	long address = syscall(__NR_my_syscall, pid, addr);
+
+	if (address == -1)
+		printf("That page is not available.\n");
+	else
+		printf("The address is %ld.", address);
+
+	return 0;
+}
+
+//int main (int argc, char* argv[])

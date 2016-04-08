3c0702a8de6fafaa823747d112f79d57cda4b866
dur1n <tqtuan1982@gmail.com>
2016-04-08 09:42:52 -0700

    changed int to unsigned long addr

--- memCall_test.c
+++ memCall_test.c
@@ -7,7 +7,7 @@
 main(int argc, char *argv[]) {
 {
     int pid = atoi(argv[1]);  // Converting a string argument to an integer 
-    int addr = atoi(argv[2]);  
+    unsigned long addr = atoi(argv[2]);  
 	long address = syscall(__NR_my_syscall, pid, addr);
 	if (address == -1)
 		printf("page is not available.\n");

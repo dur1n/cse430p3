1be5f3499574ef9d1e49a1b42533adf26815967e
dur1n <tqtuan1982@gmail.com>
2016-04-07 23:44:46 -0700

    Update memCall_test.c

--- memCall_test.c
+++ memCall_test.c
@@ -9,10 +9,9 @@ main(int argc, char *argv[]) {
     int pid = atoi(argv[1]);  // Converting a string argument to an integer 
     int addr = atoi(argv[2]);  
 	long address = syscall(__NR_my_syscall, pid, addr);
-
 	if (address == -1)
 		printf("page is not available.\n");
 	else
-		printf("PID:%d\tPhysical Address:0x%lx.\n", pid, address);
+		printf("PID:%d\tPhysical Address:0x%lx\n", pid, address);
 	return 0;
 }
